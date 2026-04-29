a843b1fa4180f4037b581d78207a39bf86901eab
paanipoorie <knishant3000@gmail.com>
2026-04-29 20:46:16 +0530

    restructuring the UI: adding home and tally pages

---
+++ src/pages/Tally.jsx
@@ -0,0 +1,12 @@
+function Tally() {
+  return (
+    <main className="page-shell">
+      <section className="brutal-panel">
+        <h1>Tally</h1>
+        <p>Heatmap and streaks will come here.</p>
+      </section>
+    </main>
+  );
+}
+
+export default Tally;
